f5a949a1d810e7b1a22b19a0207ff1f729d949bd
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2018-11-01 19:40:06 -0300

    Finaliza app

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
-const app = express()
+const request = require('request');
 const bodyParser = require('body-parser');
+const app = express()
+
+const apiKey = '020a5ad4e51f2b8eade77bd72f410510';
 
 app.set('view engine', 'ejs')
 
@@ -10,14 +13,35 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', function (req, res) {
-  res.render('index');
-});
-
-app.post('/', function (req, res) {
-  res.render('index');
-  console.log(req.body.city);
+    res.render('index', {weather: null, error: null});
 });
 
 app.listen(3000, function () {
   console.log('App rodando na porta 3000 ...!')
+})
+
+/*
+app.post('/', function (req, res) {
+  res.render('index');
+  console.log(req.body.city);
+}); */
+
+app.post('/', function (req, res) {
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+request(url, function (err, response, body) {
+	let error_msg = 'Error, tente novamente';
+    if(err){    	
+      	res.render('index', {weather: null, error: error_msg});
+    } else {
+      let weather = JSON.parse(body)
+    	if (weather.main == undefined) {
+    		console.log(weather);
+        	res.render('index', {weather: null, error: error_msg + '. Cidade não encontrada!'});
+    	} else {
+    		let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    		res.render('index', {weather: weatherText, error: null});
+    	}
+    }
+  });
 })
