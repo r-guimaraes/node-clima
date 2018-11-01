5af187c19f60bb0963a2867186aac9f219fddc49
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2018-11-01 19:21:05 -0300

    add body parser

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs')
 
 // Explicitamente dá acesso à pasta public
 app.use(express.static('public'))
 
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.get('/', function (req, res) {
   res.render('index');
-})
+});
+
+app.post('/', function (req, res) {
+  res.render('index');
+  console.log(req.body.city);
+});
 
 app.listen(3000, function () {
   console.log('App rodando na porta 3000 ...!')
